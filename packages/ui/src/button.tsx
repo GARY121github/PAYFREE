c10e8ad6dae993e4930085db9f4c14e2480601e2
gary <harshprakash000@gmail.com>
2024-04-11 02:20:05 +0530

    Initilised the project and created db connection

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -15,6 +15,7 @@ export const Button = ({ children, className, appName }: ButtonProps) => {
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
       {children}
+      <h1 className="text-3xl text-red-500">This is something new to this project</h1>
     </button>
   );
 };
